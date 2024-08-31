375e96b7f5290aef2be85db19863ec9f6aa2ba83
suhaschalak <chalaksuhas01@gmail.com>
2024-08-31 12:49:26 +0530

    web app is ready

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
-#create a simple flask application
-app=Flask(__name__)
-regmodel=pickle.load(open("regmodel1.pkl",'rb'))
-scalar=pickle.load(open("scaling.pkl",'rb'))
+# Create a simple Flask application
+app = Flask(__name__)
+regmodel = pickle.load(open("regmodel1.pkl", 'rb'))
+scalar = pickle.load(open("scaling.pkl", 'rb'))
 
 @app.route("/")
 def home():
@@ -14,13 +13,21 @@ def home():
 
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
-    data=request.json['data']
+    data = request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+    output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-if __name__=="__main__":
+@app.route('/predict', methods=['POST'])  # Corrected 'method' to 'methods'
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text=f"The house price predicted is {output}")
+
+if __name__ == "__main__":
     app.run(debug=True)
